d55f7127f7fbaf5eea7ec03967f975d97528c290
Nate Bogdanowicz <natezb@gmail.com>
2013-10-30 16:37:33 -0700

    Make common functions available via the toplevel package

--- instrumental/__init__.py
+++ instrumental/__init__.py
@@ -6,3 +6,7 @@ from pint import UnitRegistry
 # Make a single UnitRegistry instance for the entire package
 u = UnitRegistry()
 Q_ = u.Quantity
+
+# Make common functions available directly
+from .plotting import plot, param_plot
+from .fitting import guided_trace_fit, guided_ringdown_fit
