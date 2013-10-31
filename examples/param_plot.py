b5800038f6fa22f33c228a57b29c8f99682d958c
Nate Bogdanowicz <natezb@gmail.com>
2013-10-31 16:41:05 -0700

    Updated param_plot example to use units

--- examples/param_plot.py
+++ examples/param_plot.py
@@ -1,28 +1,28 @@
-from instrumental import plotting as p
+from instrumental import u, plotting as ip
 from matplotlib import pyplot as plt
 from numpy import sin, pi, arange
 
-def curve(x, amp, freq, phase):
-    return amp * sin(2*pi*freq*x + phase)
+def curve(t, amp, freq, phase):
+    return amp * sin(2*pi*u.rad*freq*t + phase)
 
+# param_plot supports unitful parameters and variables
 params = {
-    'freq': 3,
+    'freq': 3*u.Hz,
     'amp': {
-        'min':-10,
-        'max':10,
-        'init':5
+        'min':-10*u.V,
+        'max':10*u.V,
+        'init':5*u.V
     },
     'phase': {
-        'min':0,
-        'max':2*pi,
-        'init':pi
+        'min':0*u.rad,
+        'max':2*pi*u.rad,
+        'init':pi*u.rad
     }
 }
 
-t = arange(0.0, 1.0, 0.001)
-p.param_plot(t, curve, params)
-plt.gca().plot(t, t)
-plt.xlabel("X Label")
-plt.ylabel("Y Label")
-plt.title("Title")
+t = arange(0.0, 1.0, 0.001) * u.s
+ip.param_plot(t, curve, params)
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude [V]")
+plt.title("Parameter Plot")
 plt.show()
