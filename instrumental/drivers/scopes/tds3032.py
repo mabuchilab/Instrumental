185c686918c51e51fe7a409dfc222347c42c1706
Nate Bogdanowicz <natezb@gmail.com>
2013-12-13 14:32:59 -0800

    Added function to create scope object from VISA address, and implemented binary data transfer from Tektronix scopes.

--- instrumental/drivers/scopes/tds3032.py
+++ instrumental/drivers/scopes/tds3032.py
@@ -7,18 +7,22 @@ Driver module for Tektronix TDS3032 oscilloscopes.
 import numpy as np
 import visa
 from instrumental import u
+from . import Scope
 
-class TDS_3032(object):
+class TDS_3032(Scope):
     """
     A Tektronix TDS3032 oscilloscope.
     """
-    def __init__(self, name):
+    def __init__(self, name=None, instrument=None):
         """
         Create a scope object that has the given VISA name *name* and connect
         to it. You can find available instrument names using the VISA
         Instrument Manager.
         """
-        self.inst = visa.instrument(name)
+        if instrument:
+            self.inst = instrument
+        else:
+            self.inst = visa.instrument(name)
 
     def get_data(self, channel=1):
         """
@@ -28,39 +32,31 @@ class TDS_3032(object):
         inst = self.inst
         
         inst.write("data:source ch{}".format(channel))
+        stop = int(inst.ask("wfmpre:nr_pt?")) # Get source's number of points
         inst.write("data:width 2")
-        inst.write("data:encdg ascii")
+        inst.write("data:encdg RIBinary")
         inst.write("data:start 1")
-        inst.write("data:stop 10000")
+        inst.write("data:stop {}".format(stop))
         
-        raw_asc = inst.ask("curve?")
+        raw_bin = inst.ask("curve?")
 
         # Get scale and offset factors
         x_scale = float(inst.ask("wfmpre:xincr?"))
         y_scale = float(inst.ask("wfmpre:ymult?"))
         x_zero = float(inst.ask("wfmpre:xzero?"))
         y_zero = float(inst.ask("wfmpre:yzero?"))
+        x_offset = float(inst.ask("wfmpre:pt_off?"))
         y_offset = float(inst.ask("wfmpre:yoff?"))
         
         x_unit = inst.ask("wfmpre:xunit?")[1:-1]
         y_unit = inst.ask("wfmpre:yunit?")[1:-1]
         
-        raw_data_x = np.arange(1, 10001)
-        raw_data_y = np.fromstring(raw_asc, sep=",")
+        raw_data_x = np.arange(1, stop+1)
+
+        header_size = int(raw_bin[1]) + 2
+        raw_data_y = np.frombuffer(raw_bin, dtype='>i2', offset=header_size)
         
-        data_x = u[x_unit] * ((raw_data_x)*x_scale + x_zero)
+        data_x = u[x_unit] * ((raw_data_x - x_offset)*x_scale + x_zero)
         data_y = u[y_unit] * ((raw_data_y - y_offset)*y_scale + y_zero)
         
         return data_x, data_y
-
-
-# List of pre-defined instruments
-SCOPE_A = TDS_3032("TCPIP::171.64.84.116::INSTR")
-
-
-if __name__=="__main__":
-    from pylab import plot
-    scope = SCOPE_A
-    x,y = scope.get_data(1)
-    plot(x, y)
-    print("Done")
