759b22c03fdace7e7cdeb9f160932b8d53543801
Nate Bogdanowicz <natezb@gmail.com>
2014-07-09 11:30:29 -0700

    Fixed sphinx theme on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,15 @@
 # serve to show the default.
 
 import sys, os
-import sphinx_rtd_theme
+
+# Code taken from https://github.com/snide/sphinx_rtd_theme
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+# Only import and specify the rtd theme if we're building locally
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -102,7 +110,6 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -110,7 +117,6 @@ html_theme = "sphinx_rtd_theme"
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
