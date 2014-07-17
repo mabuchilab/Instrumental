f0078a4d161d843ef2b9a57528b275f4f3e54039
Nate Bogdanowicz <natezb@gmail.com>
2014-07-16 17:10:39 -0700

    More improvements to documentation in newport.py

--- instrumental/drivers/powermeters/newport.py
+++ instrumental/drivers/powermeters/newport.py
@@ -26,14 +26,14 @@ class Newport_1830_C(PowerMeter):
     """A Newport 1830-C power meter"""
 
     # Status byte codes
-    PARAM_ERROR = 1
-    COMMAND_ERROR = 2
-    SATURATED = 4
-    OUT_OF_RANGE = 8
-    MSG_AVAILABLE = 16
-    BUSY = 32
-    SERVICE_REQUEST = 64
-    READY_READING = 128
+    _PARAM_ERROR = 1
+    _COMMAND_ERROR = 2
+    _SATURATED = 4
+    _OUT_OF_RANGE = 8
+    _MSG_AVAILABLE = 16
+    _BUSY = 32
+    _SERVICE_REQUEST = 64
+    _READY_READING = 128
 
     # Filter averaging constants
     SLOW_FILTER = 1
@@ -44,15 +44,12 @@ class Newport_1830_C(PowerMeter):
         self._inst = inst
 
     def get_status_byte(self):
-        """
-        Queries the status byte register, and returns the result as an integer.
-        """
+        """Query the status byte register and return it as an int"""
         status = self._inst.ask('Q?')
         return int(status)
 
     def get_power(self):
-        """
-        Returns the current power measurement.
+        """Get the current power measurement
 
         Returns
         -------
@@ -71,11 +68,11 @@ class Newport_1830_C(PowerMeter):
         return Q_(power, 'watts')
 
     def enable_auto_range(self):
-        """Enables auto-range."""
+        """Enable auto-range"""
         self.set_range(0)
 
     def disable_auto_range(self):
-        """Disables auto-range.
+        """Disable auto-range
 
         Leaves the signal range at its current position.
         """
@@ -83,7 +80,7 @@ class Newport_1830_C(PowerMeter):
         self.set_range(cur_range)
 
     def set_range(self, range_num):
-        """ Sets the range for power measurements (amplifier gain)
+        """Set the range for power measurements
 
         range_num = 0 for auto-range
         range_num = 1 to 8 for manual signal range
@@ -97,12 +94,12 @@ class Newport_1830_C(PowerMeter):
         self._inst.write('R{}'.format(int(range_num)))
 
     def get_range(self):
-        """ Returns the current range setting as an int.
+        """Return the current range setting as an int
 
-        n=1 corresponds to the lowest range, while n=8 is the highest range
-        (least amplifier gain)
+        1 corresponds to the lowest range, while 8 is the highest range (least
+        amplifier gain).
 
-        Note that this does not query the status of auto-range
+        Note that this does not query the status of auto-range.
 
         Returns
         -------
@@ -113,24 +110,31 @@ class Newport_1830_C(PowerMeter):
         return int(val)
 
     def set_wavelength(self, wavelength):
-        """ Set the wavelength of the input signal. """
+        """Set the input signal wavelength setting
+
+        Parameters
+        ----------
+        wavelength : Quantity
+            wavelength of the input signal, in units of [length]
+        """
         wavelength = int(Q_(wavelength).to('nm').magnitude)
         self._inst.write("W{}".format(wavelength))
 
     def get_wavelength(self):
-        """ Returns the wavelength setting of the meter """
+        """Get the input wavelength setting"""
         val = int(self._inst.ask("W?"))
         return Q_(val, 'nm')
 
     def enable_attenuator(self, enabled=True):
-        """Set whether the power meter attenuator setting is enabled"""
+        """Enable the power meter attenuator"""
         self._inst.write('A{}'.format(int(enabled)))
 
     def disable_attenuator(self):
+        """Disable the power meter attenuator"""
         self.enable_attenuator(False)
 
     def attenuator_enabled(self):
-        """Returns whether the attenuator is enabled.
+        """Whether the attenuator is enabled
 
         Returns
         -------
@@ -141,44 +145,44 @@ class Newport_1830_C(PowerMeter):
         return bool(val)
 
     def set_slow_filter(self):
-        """Sets averaging filter to slow mode.
+        """Set the averaging filter to slow mode
 
-        The slow filter uses a 16-measurement average.
+        The slow filter uses a 16-measurement running average.
         """
         self._inst.write('F1')
 
     def set_medium_filter(self):
-        """Sets averaging filter to medium mode.
+        """Set the averaging filter to medium mode
 
-        The medium filter uses a 4-measurement average.
+        The medium filter uses a 4-measurement running average.
         """
         self._inst.write('F2')
 
     def set_no_filter(self):
-        """Sets averaging filter to fast mode, i.e. no averaging."""
+        """Set the averaging filter to fast mode, i.e. no averaging"""
         self._inst.write('F3')
 
     def get_filter(self):
-        """ Returns the current setting for the filter.
+        """Get the current setting for the averaging filter
 
         Returns
         -------
-        FAST_FILTER, MEDIUM_FILTER, NO_FILTER
-            mode of the averaging filter
+        SLOW_FILTER, MEDIUM_FILTER, NO_FILTER
+            the current averaging filter
         """
         val = self._inst.ask("F?")
         return int(val)
 
     def enable_hold(self, enable=True):
-        """Sets whether the 'hold' mode is enabled."""
+        """Enable hold mode"""
         self._inst.write('G{}'.format(int(not enable)))
 
     def disable_hold(self):
-        """Disables 'hold' mode, enabling 'run' mode."""
+        """Disable hold mode"""
         self.enable_hold(False)
 
     def hold_enabled(self):
-        """Whether 'hold' mode is enabled.
+        """Whether hold mode is enabled
 
         Returns
         -------
@@ -189,27 +193,28 @@ class Newport_1830_C(PowerMeter):
         return (val == 0)
 
     def is_measurement_valid(self):
-        """Checks if the current measurement is valid.
+        """Whether the current measurement is valid
 
         The measurement is considered invalid if the power meter is saturated,
         over-range or busy.
         """
         reg = self.get_status_byte()
-        is_saturated = bool(reg & self.SATURATED)
-        is_over_range = bool(reg & self.OUT_OF_RANGE)
-        is_busy = bool(reg & self.BUSY)
+        is_saturated = bool(reg & self._SATURATED)
+        is_over_range = bool(reg & self._OUT_OF_RANGE)
+        is_busy = bool(reg & self._BUSY)
 
         return not (is_saturated or is_over_range or is_busy)
 
     def store_reference(self):
-        """
+        """Store the current power input as a reference
+
         Sets the current power measurement as the reference power for future dB
         or relative measurements.
         """
         self._inst.write('S')
 
     def enable_zero(self, enable=True):
-        """Enable/disable the zero function.
+        """Enable the zero function
 
         When enabled, the next power reading is stored as a background value
         and is subtracted off of all subsequent power readings.
@@ -217,16 +222,16 @@ class Newport_1830_C(PowerMeter):
         self._inst.write("Z{}".format(int(enable)))
 
     def disable_zero(self):
-        """Disable the zero function."""
+        """Disable the zero function"""
         self.enable_zero(False)
 
     def zero_enabled(self):
-        """Whether the zero function is enabled."""
+        """Whether the zero function is enabled"""
         val = int(self._inst.ask('Z?'))  # Need to cast to int first
         return bool(val)
 
     def set_units(self, units):
-        """ Sets the units for displaying power measurements.
+        """Set the units for displaying power measurements
 
         The different unit modes are watts, dB, dBm, and REL. Each displays
         the power in a different way.
@@ -258,12 +263,12 @@ class Newport_1830_C(PowerMeter):
         self._inst.write('U{}'.format(valid_units[units]))
 
     def get_units(self):
-        """Get the units used for power measurments.
+        """Get the units used for displaying power measurements
 
         Returns
         -------
         units : str
-            One of 'watts', 'db', 'dbm', 'rel'
+            'watts', 'db', 'dbm', or 'rel'
         """
         val = int(self._inst.ask('U?'))
         units = {1: 'watts', 2: 'db', 3: 'dbm', 4: 'rel'}
