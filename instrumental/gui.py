6e00f554c6214fe4f2b5313d612cdd4194451500
Nate Bogdanowicz <natezb@gmail.com>
2015-12-08 21:22:36 -0800

    Overhauled CameraView to use newly standardized Camera API methods

--- instrumental/gui.py
+++ instrumental/gui.py
@@ -1,6 +1,7 @@
+import numpy as np
 import scipy.misc
-from PySide.QtCore import Qt, QTimer, Signal
-from PySide.QtGui import QGraphicsView, QGraphicsScene, QPixmap, QImage, QLabel
+from PySide.QtCore import QTimer
+from PySide.QtGui import QPixmap, QImage, QLabel
 
 mpl, FigureCanvas, Figure = None, None, None
 def load_matplotlib():
@@ -25,43 +26,46 @@ class CameraView(QLabel):
     def __init__(self, camera=None):
         super(CameraView, self).__init__()
         self.camera = camera
+        self._cmin = 0
+        self._cmax = None
 
-    def grab_frame(self):
-        self.camera.freeze_frame()
-        self._set_pixmap_from_camera()
+    def grab_image(self):
+        arr = self.camera.grab_image()
+        self._set_pixmap_from_array(arr)
 
-    def start_video(self, framerate=None):
+    def start_video(self):
         timer = QTimer()
         self.timer = timer
         timer.timeout.connect(self._wait_for_frame)
-        self.camera.start_live_video(framerate)
-        timer.start(int(1000./self.camera.framerate))
+        self.camera.start_live_video()
+        timer.start(0)  # Run full throttle
 
     def stop_video(self):
         self.timer.stop()
         self.camera.stop_live_video()
 
-    def _set_pixmap_from_camera(self):
-        bpl = self.camera.bytes_per_line
-        arr = self.camera.image_array()
+    def _set_pixmap_from_array(self, arr):
+        bpl = arr.strides[0]
+        is_rgb = len(arr.shape) == 3
 
-        if self.camera.color_mode == 'RGB32':
-            buf = self.camera.image_buffer()  # TODO: Fix to use image_array() instead
+        if is_rgb and arr.dtype == np.uint8:
             format = QImage.Format_RGB32
-            image = QImage(buf, self.camera.width, self.camera.height, bpl, format)
-        elif self.camera.color_mode == 'mono8':
+            image = QImage(arr.data, self.camera.width, self.camera.height, bpl, format)
+        elif not is_rgb and arr.dtype == np.uint8:
             # TODO: Somehow need to make sure data is ordered as I'm assuming
             format = QImage.Format_Indexed8
             image = QImage(arr.data, self.camera.width, self.camera.height, bpl, format)
             self._saved_img = arr
-        elif self.camera.color_mode == 'mono16':
-            pil_img = scipy.misc.toimage(arr)  # Normalize values to fit in uint8
+        elif not is_rgb and arr.dtype == np.uint16:
+            if not self._cmax:
+                self._cmax = arr.max()  # Set cmax once from first image
+            arr = scipy.misc.bytescale(arr, self._cmin, self._cmax)
             format = QImage.Format_Indexed8
-            data = pil_img.tostring()
-            image = QImage(data, pil_img.size[0], pil_img.size[1], pil_img.size[0], format)
-            self._saved_img = data  # Save a reference to keep Qt from crashing
+            w, h = self.camera.width, self.camera.height
+            image = QImage(arr.data, w, h, w, format)
+            self._saved_img = arr  # Save a reference to keep Qt from crashing
         else:
-            raise Exception("Unsupported color mode '{}'".format(self.camera.color_mode))
+            raise Exception("Unsupported color mode")
 
         self.setPixmap(QPixmap.fromImage(image))
         pixmap_size = self.pixmap().size()
@@ -69,9 +73,10 @@ class CameraView(QLabel):
             self.setMinimumSize(self.pixmap().size())
 
     def _wait_for_frame(self):
-        frame_ready = self.camera.wait_for_frame(0)
+        frame_ready = self.camera.wait_for_frame(timeout='0 ms')
         if frame_ready:
-            self._set_pixmap_from_camera()
+            arr = self.camera.latest_frame(copy=False)
+            self._set_pixmap_from_array(arr)
 
     def set_height(self, h):
         """ Sets the height while keeping the image aspect ratio fixed """
