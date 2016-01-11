aa849179c0ce49ad13a912a3d4c5514986ab904c
Nate Bogdanowicz <natezb@gmail.com>
2016-01-11 15:58:12 -0800

    Use the right directories when doing a cffi build step from setup.py

--- instrumental/drivers/cameras/_pixelfly/build_errortext.py
+++ instrumental/drivers/cameras/_pixelfly/build_errortext.py
@@ -1,12 +1,13 @@
+import os.path
 from cffi import FFI
 
 ffi = FFI()
-ffi.set_source('errortext', """
+ffi.set_source('instrumental.drivers.cameras._pixelfly.errortext', """
     #define PCO_ERR_H_CREATE_OBJECT
     #define PCO_ERRT_H_CREATE_OBJECT
     #include <windows.h>
     #include "PCO_errt.h"
-""")
+""", include_dirs=[os.path.dirname(__file__)])
 ffi.cdef("void PCO_GetErrorText(DWORD dwerr, char* pbuf, DWORD dwlen);")
 
 if __name__ == '__main__':
