5e22fbb60e16bcc2fd12f305f5a696623e1fbcf8
Nate Bogdanowicz <natezb@gmail.com>
2016-01-12 19:16:45 -0800

    Use data_files instead of post_install script

--- setup.py
+++ setup.py
@@ -17,6 +17,14 @@ classifiers = [
     'Programming Language :: Python :: 2.7',
 ]
 
+# Get user data directory without having to import appdirs submodule
+base_dir = os.path.dirname(__file__)
+appdirs_names = {}
+with open(os.path.join(base_dir, 'instrumental', 'appdirs.py')) as f:
+    exec(f.read(), appdirs_names)
+user_data_dir = appdirs_names['user_data_dir']
+
+
 # Check for cffi
 try:
     import cffi
@@ -42,6 +50,8 @@ if __name__ == '__main__':
         version = version,
         packages = find_packages(exclude=['*._cffi_build']),
         package_data = {'': ['*.h', '*.pyd']},
+        data_files = [(user_data_dir(name, 'MabuchiLab'),
+                       [os.path.join('data', 'instrumental.conf')])],
         author = author,
         author_email = "natezb@stanford.edu",
         description = description,
@@ -55,7 +65,3 @@ if __name__ == '__main__':
     if not build_cffi_modules:
         print("\nPython cffi is not installed, so CFFI modules were not built. If you would like "
               "to use cffi-based drivers, first install cffi, then rebuild Instrumental")
-
-    if 'install' in sys.argv:
-        print("\nIf this is your first time installing Instrumental, now run "
-              "`python post_install.py` to install the config file")
