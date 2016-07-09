3184dc183c06366253011dece717ed6d1d95f1ba
Nate Bogdanowicz <natezb@gmail.com>
2016-07-09 16:37:23 -0700

    Build dummy module for `errortext` on non-Windows platforms

--- instrumental/drivers/cameras/_pixelfly/_cffi_build/build_errortext.py
+++ instrumental/drivers/cameras/_pixelfly/_cffi_build/build_errortext.py
@@ -1,14 +1,20 @@
+import sys
 import os.path
 from cffi import FFI
 
 ffi = FFI()
-ffi.set_source('instrumental.drivers.cameras._pixelfly.errortext', """
-    #define PCO_ERR_H_CREATE_OBJECT
-    #define PCO_ERRT_H_CREATE_OBJECT
-    #include <windows.h>
-    #include "PCO_errt.h"
-""", include_dirs=[os.path.dirname(__file__)])
-ffi.cdef("void PCO_GetErrorText(DWORD dwerr, char* pbuf, DWORD dwlen);")
+mod_name = 'instrumental.drivers.cameras._pixelfly.errortext'
+
+if sys.platform.startswith('win'):
+    ffi.set_source(mod_name, """
+        #define PCO_ERR_H_CREATE_OBJECT
+        #define PCO_ERRT_H_CREATE_OBJECT
+        #include <windows.h>
+        #include "PCO_errt.h"
+    """, include_dirs=[os.path.dirname(__file__)])
+    ffi.cdef("void PCO_GetErrorText(DWORD dwerr, char* pbuf, DWORD dwlen);")
+else:
+    ffi.set_source(mod_name, '')
 
 if __name__ == '__main__':
     ffi.compile()
