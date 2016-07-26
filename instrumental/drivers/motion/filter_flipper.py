1ca8646c988ec919dec78886cc97de8ffa9f0663
Christopher Rogers <cmrogers@stanford.edu>
2016-07-25 20:46:47 -0700

    Added error checking to the filter flipper driver.

--- instrumental/drivers/motion/filter_flipper.py
+++ instrumental/drivers/motion/filter_flipper.py
@@ -16,7 +16,7 @@ from nicelib import NiceLib, NiceObjectDef
 from . import Motion
 from .. import _ParamDict
 from ... import Q_
-from ...errors import InstrumentTypeError
+from ...errors import InstrumentTypeError, Error
 from ..util import check_units, check_enums
 
 FILTER_FLIPPER_TYPE = 37
@@ -39,6 +39,7 @@ ffi.cdef("""
       SAFEARRAYBOUND rgsabound[1];
     } SAFEARRAY, *LPSAFEARRAY;
 """)
+
 
 with open(os.path.join(os.path.dirname(__file__), '_filter_flipper', 'FilterFlipper.h')) as f:
     ffi.cdef(f.read())
@@ -72,52 +73,62 @@ def list_instruments():
 class NiceFilterFlipper(NiceLib):
     """ Provides a convenient low-level wrapper for the library
     Thorlabs.MotionControl.FilterFlipper.dll"""
-    _ret_wrap = None
+    _ret_wrap = 'error_code'
     _struct_maker = None
     _ffi = ffi
     _lib = lib
     _prefix = ('FF_', 'TLI_')
     _buflen = 512
 
+    def _ret_bool_error_code(retval):
+        retval = bool(retval)
+        if retval==False:
+            raise FilterFlipperError('The function did not execute successfully')
+
+    def _ret_error_code(retval):
+        if not (retval == 0 or retval==None):
+            raise FilterFlipperError(error_dict[retval])      
+
     BuildDeviceList = ()
-    GetDeviceListSize = ()
+    GetDeviceListSize = ({'ret': 'return'},)
     GetDeviceList = ('out')
     GetDeviceListByType = ('out', 'in')
-# This function seemed not to be properly implemented in the .dll
-#    GetDeviceListByTypes = ('out', 'in', 'in')
+    GetDeviceInfo = ('in', 'out', {'ret': 'bool_error_code'})
+
+    # GetDeviceListByTypes seemed not to be properly implemented in the .dll
+    #    GetDeviceListByTypes = ('out', 'in', 'in')
     GetDeviceListExt = ('buf', 'len')
     GetDeviceListByTypeExt = ('buf', 'len', 'in')
     GetDeviceListByTypesExt = ('buf', 'len', 'in', 'in')
     
     Flipper = NiceObjectDef({
-        'GetDeviceInfo' : ('in', 'out'),
         'Open' : ('in'),
         'Close' : ('in'),
         'Identify' : ('in'),
         'GetHardwareInfo' : ('in', 'buf', 'len', 'out', 'out', 'buf', 'len',
                               'out', 'out', 'out'),
-        'GetFirmwareVersion' : ('in'),
-        'GetSoftwareVersion' : ('in'),
-        'LoadSettings' : ('in'),
-        'PersistSettings' : ('in'),
-        'GetNumberPositions' : ('in'),
+        'GetFirmwareVersion' : ('in', {'ret': 'return'}),
+        'GetSoftwareVersion' : ('in', {'ret': 'return'}),
+        'LoadSettings' : ('in', {'ret': 'bool_error_code'}),
+        'PersistSettings' : ('in', {'ret': 'bool_error_code'}),
+        'GetNumberPositions' : ('in', {'ret': 'return'}),
         'Home' : ('in'),
         'MoveToPosition' : ('in', 'in'),
-        'GetPosition' : ('in'),
+        'GetPosition' : ('in', {'ret': 'return'}),
         'GetIOSettings' : ('in', 'out'),
-        'GetTransitTime' : ('in'),
+        'GetTransitTime' : ('in', {'ret': 'return'}),
         'SetTransitTime' : ('in', 'in'),
         'RequestStatus' : ('in'),
-        'GetStatusBits' : ('in'),
-        'StartPolling' : ('in', 'in'),
-        'PollingDuration' : ('in'),
+        'GetStatusBits' : ('in', {'ret': 'return'}),
+        'StartPolling' : ('in', 'in', {'ret': 'bool_error_code'}),
+        'PollingDuration' : ('in', {'ret': 'return'}),
         'StopPolling' : ('in'),
         'RequestSettings' : ('in'),
         'ClearMessageQueue' : ('in'),
         'RegisterMessageCallback' : ('in', 'in'),
-        'MessageQueueSize' : ('in'),
-        'GetNextMessage' : ('in', 'in', 'in', 'in'),
-        'WaitForMessage' : ('in', 'in', 'in', 'in'),
+        'MessageQueueSize' : ('in',  {'ret': 'return'}),
+        'GetNextMessage' : ('in', 'in', 'in', 'in', {'ret': 'bool_error_code'}),
+        'WaitForMessage' : ('in', 'in', 'in', 'in', {'ret': 'bool_error_code'}),
     })
 
 
@@ -261,3 +272,30 @@ class Filter_Flipper(Motion):
         transit_time: pint quantity with units of time """
         transit_time = transit_time.to('ms').magnitude
         return int(self._NiceFF.SetTransitTime(transit_time))
+class FilterFlipperError(Error):
+    pass
+
+error_dict = {0: 'OK - Success  ',
+              1: 'InvalidHandle - The FTDI functions have not been initialized.',
+              2: 'DeviceNotFound - The Device could not be found.',  
+              3: 'DeviceNotOpened - The Device must be opened before it can be accessed ',
+              4: 'IOError - An I/O Error has occured in the FTDI chip.',
+              5: 'InsufficientResources - There are Insufficient resources to run this application.',
+              6: 'InvalidParameter - An invalid parameter has been supplied to the device.' , 
+              7: 'DeviceNotPresent - The Device is no longer present',
+              8: 'IncorrectDevice - The device detected does not match that expected./term>',
+              32: 'ALREADY_OPEN - Attempt to open a device that was already open.',
+              33: 'NO_RESPONSE - The device has stopped responding.',
+              34: 'NOT_IMPLEMENTED - This function has not been implemented.', 
+              35: 'FAULT_REPORTED - The device has reported a fault.',
+              36: 'INVALID_OPERATION - The function could not be completed at this time.',
+              36: 'DISCONNECTING - The function could not be completed because the device is disconnected.',
+              41: 'FIRMWARE_BUG - The firmware has thrown an error.',
+              42: 'INITIALIZATION_FAILURE - The device has failed to initialize',
+              43: 'INVALID_CHANNEL - An Invalid channel address was supplied.',
+              37: 'UNHOMED - The device cannot perform this function until it has been Homed.',
+              38: 'INVALID_POSITION - The function cannot be performed as it would result in an illegal position.',
+              39: 'INVALID_VELOCITY_PARAMETER - An invalid velocity parameter was supplied',
+              44: 'CANNOT_HOME_DEVICE - This device does not support Homing ',
+              45: 'TL_JOG_CONTINOUS_MODE - An invalid jog mode was supplied for the jog function.'
+}
