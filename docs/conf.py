2bade8737a7799419ee7d1cb3474baf08d8831ce
Nate Bogdanowicz <natezb@gmail.com>
2017-06-23 17:00:07 -0700

    Switch to using napoleon with sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,8 @@ def setup(app):
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
-              'sphinx.ext.coverage', 'sphinx.ext.imgmath', 'sphinx.ext.autosummary', 'numpydoc']
+              'sphinx.ext.coverage', 'sphinx.ext.imgmath', 'sphinx.ext.autosummary',
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
