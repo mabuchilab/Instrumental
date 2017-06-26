ebd7012553b59e7217ed9518c60c94f911777274
Nate Bogdanowicz <natezb@gmail.com>
2017-06-26 13:13:10 -0700

    Search for uEye headers as well
    This new syntax requires the current development version of
    NiceLib (beyond 0.3.1).

--- instrumental/drivers/cameras/_build_uc480.py
+++ instrumental/drivers/cameras/_build_uc480.py
@@ -4,15 +4,24 @@ from nicelib import build_lib
 from nicelib.process import declspec_hook
 
 header_info = {
-    'win*': {
-        'path': (
-            r"{PROGRAMFILES(X86)}\Thorlabs\DCx Cameras\Develop\include",
-            r"{PROGRAMFILES}\Thorlabs\DCx Cameras\Develop\include",
-            r"{PROGRAMFILES(X86)}\Thorlabs\Scientific Imaging\DCx Camera Support\Develop\Include",
-            r"{PROGRAMFILES}\Thorlabs\Scientific Imaging\DCx Camera Support\Develop\Include",
-        ),
-        'header': 'uc480.h'
-    },
+    'win*': (
+        {
+            'path': (
+                r"{PROGRAMFILES(X86)}\Thorlabs\DCx Cameras\Develop\include",
+                r"{PROGRAMFILES}\Thorlabs\DCx Cameras\Develop\include",
+                r"{PROGRAMFILES(X86)}\Thorlabs\Scientific Imaging\DCx Camera Support\Develop\Include",
+                r"{PROGRAMFILES}\Thorlabs\Scientific Imaging\DCx Camera Support\Develop\Include",
+            ),
+            'header': 'uc480.h'
+        },
+        {
+            'path': (
+                r"{PROGRAMFILES(X86)}\IDS\uEye\Develop\include",
+                r"{PROGRAMFILES}\IDS\uEye\Develop\include",
+            ),
+            'header': 'uEye.h'
+        }
+    ),
 }
 
 lib_names = {
