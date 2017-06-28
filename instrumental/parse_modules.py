c72fa40e001da9e0051688a72eaee9a3bdec1cce
Nate Bogdanowicz <natezb@gmail.com>
2017-06-28 13:27:10 -0700

    Switch to using `ast` for parsing driver imports and special vars

--- instrumental/parse_modules.py
+++ instrumental/parse_modules.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Nate Bogdanowicz
-import re
 import os
 import os.path
+import ast
 import datetime as dt
+import logging as log
 
 from . import std_modules
 
 THIS_DIR = os.path.split(__file__)[0] or '.'
 DEFAULT_PRIORITY = 5
 
-RE_IMPORT = re.compile(r"^import[ ]+(\w+)", re.MULTILINE)
-RE_FROM_IMPORT = re.compile(r"^from[ ]+([^.][^ ]*)[ ]+import", re.MULTILINE)
-RE_PARAMS = re.compile(r"_INST_PARAMS[ ]*=[ ]*(\[[^\]]*\])")
-RE_PRIORITY = re.compile(r"_INST_PRIORITY[ ]*=[ ]*(.*)")
+IGNORED_IMPORTS = ['numpy', 'scipy', 'pint', 'future', 'past']
+VAR_NAMES = ['_INST_PARAMS', '_INST_PRIORITY']
 
 
 def load_module_source(module):
@@ -23,31 +22,18 @@ def load_module_source(module):
     return source
 
 
-def parse_priority(source):
-    match = re.search(RE_PRIORITY, source)
-    return eval(match.group(1), {}) if match else DEFAULT_PRIORITY
-
-
-def parse_params(source):
-    match = re.search(RE_PARAMS, source)
-    return eval(match.group(1), {}) if match else None
-
-
-def parse_imported_modules(source):
-    """Parse the toplevel modules imported by the given driver module"""
-    full_modules = re.findall(RE_IMPORT, source) + re.findall(RE_FROM_IMPORT, source)
-    modules = [fullpkg.split('.', 1)[0] for fullpkg in full_modules]
-    return modules
-
-
-def get_nonstd_modules(source, ignore=['numpy', 'scipy', 'pint', 'future', 'past']):
+def filter_std_modules(imports, ignore=IGNORED_IMPORTS):
     ignore = ignore + std_modules.all
-    imported_modules = parse_imported_modules(source)
     nonstd_modules = []
-    for module in imported_modules:
+    for module in imports:
         if module not in ignore and module not in nonstd_modules:
             nonstd_modules.append(module)
     return nonstd_modules
+
+
+def driver_groups():
+    return [g for g in os.listdir(os.path.join(THIS_DIR, 'drivers'))
+            if os.path.isdir(os.path.join(THIS_DIR, 'drivers', g)) and not g.startswith('_')]
 
 
 def list_drivers():
@@ -56,16 +42,48 @@ def list_drivers():
         for fname in os.listdir(group_dir):
             if fname.endswith('.py') and not fname.startswith('_'):
                 mod_name = fname[:-3]
-                source = load_module_source(group + '.' + mod_name)
-                yield (group, mod_name, source)
+                yield (group, mod_name)
+
+
+def parse_module(module_name):
+    """Parse special vars and imports from the given driver module"""
+    source = load_module_source(module_name)
+    values = {}
+    root = ast.parse(source)
+
+    assignments = (n for n in root.body if isinstance(n, ast.Assign) and len(n.targets) == 1)
+    for assignment in assignments:
+        target = assignment.targets[0]
+        if isinstance(target, ast.Name):
+            var_name = target.id
+            if var_name in VAR_NAMES:
+                try:
+                    values[var_name] = ast.literal_eval(assignment.value)
+                except:
+                    log.info("Failed to eval value of %s in module '%s'", var_name, module_name)
+
+    imports = []
+    for node in root.body:
+        if isinstance(node, ast.Import):
+            imports.extend(n.name for n in node.names)
+        elif isinstance(node, ast.ImportFrom):
+            # Ignore dot-prefixed imports
+            if node.level == 0:
+                imports.append(node.module)
+        else:
+            continue
+    imports = [fullpkg.split('.', 1)[0] for fullpkg in imports if fullpkg is not None]
+
+    return values, imports
 
 
 def generate_info_file():
     mod_info = []
-    for group, mod_name, source in list_drivers():
-        params = parse_params(source)
-        priority = parse_priority(source)
-        nonstd_modules = get_nonstd_modules(source)
+    for group, mod_name in list_drivers():
+        values, imports = parse_module(group + '.' + mod_name)
+        params = values.get('_INST_PARAMS', None)
+        priority = values.get('_INST_PRIORITY', DEFAULT_PRIORITY)
+        nonstd_modules = filter_std_modules(imports)
         mod_info.append( (priority, (group, mod_name), params, nonstd_modules) )
 
     mod_info.sort()
@@ -88,11 +106,6 @@ def generate_info_file():
         for _, (group, mod), _, nonstd_modules in mod_info:
             f.write("    ('{}.{}', {!r}),\n".format(group, mod, nonstd_modules))
         f.write('])\n')
-
-
-def driver_groups():
-    return [g for g in os.listdir(os.path.join(THIS_DIR, 'drivers'))
-            if os.path.isdir(os.path.join(THIS_DIR, 'drivers', g)) and not g.startswith('_')]
 
 
 if __name__ == '__main__':
