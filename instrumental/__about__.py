3ba959d89929a9ccc56fea42136f5a3efad95de6
Nate Bogdanowicz <natezb@gmail.com>
2017-11-13 18:41:43 -0800

    Return to dev

--- instrumental/__about__.py
+++ instrumental/__about__.py
@@ -4,7 +4,7 @@
 import datetime
 
 __distname__ = "Instrumental-lib"
-__version__ = "0.4"
+__version__ = "0.5.dev0"
 __author__ = "Nate Bogdanowicz"
 __email__ = "natezb@gmail.com"
 __url__ = 'https://github.com/mabuchilab/Instrumental'
