274c4f32eede93a032ce49aeba2cf75dbbb015c7
Nate Bogdanowicz <natezb@gmail.com>
2017-12-14 15:51:11 -0800

    Regenerate driver_info

--- instrumental/driver_info.py
+++ instrumental/driver_info.py
@@ -80,7 +80,7 @@ driver_info = OrderedDict([
         'params': ['visa_address'],
         'classes': [],
         'imports': ['visa', 'pyvisa'],
-        'visa_info': {'TDS_2000': ('TEKTRONIX', ['TDS 2002B', 'TDS 2004B', 'TDS 2012B', 'TDS 2014B', 'TDS 2022B', 'TDS 2024B']), 'TDS_1000': ('TEKTRONIX', ['TDS 1001B', 'TDS 1002B', 'TDS 1012B']), 'TDS_3000': ('TEKTRONIX', ['TDS 3032', 'TDS 3034B']), 'MSO_DPO_4000': ('TEKTRONIX', ['MSO4034', 'DPO4034']), 'TDS_200': ('TEKTRONIX', ['TDS 210'])},
+        'visa_info': {'TDS_2000': ('TEKTRONIX', ['TDS 2002B', 'TDS 2004B', 'TDS 2012B', 'TDS 2014B', 'TDS 2022B', 'TDS 2024B']), 'TDS_1000': ('TEKTRONIX', ['TDS 1001B', 'TDS 1002B', 'TDS 1012B']), 'TDS_3000': ('TEKTRONIX', ['TDS 3012', 'TDS 3012B', 'TDS 3012C', 'TDS 3014', 'TDS 3014B', 'TDS 3014C', 'TDS 3032', 'TDS 3032B', 'TDS 3032C', 'TDS 3034', 'TDS 3034B', 'TDS 3034C', 'TDS 3052', 'TDS 3052B', 'TDS 3052C', 'TDS 3054', 'TDS 3054B', 'TDS 3054C']), 'MSO_DPO_4000': ('TEKTRONIX', ['MSO4032', 'DPO4032', 'MSO4034', 'DPO4034', 'MSO4054', 'DPO4054', 'MSO4104', 'DPO4104']), 'TDS_200': ('TEKTRONIX', ['TDS 210', 'TDS 220', 'TDS 224'])},
     }),
     ('spectrometers.bristol', {
         'params': ['port'],
@@ -91,6 +91,12 @@ driver_info = OrderedDict([
         'params': ['serial', 'usb', 'model'],
         'classes': ['CCS'],
         'imports': ['visa', 'cffi', 'nicelib'],
+    }),
+    ('tempcontrollers.covesion', {
+        'params': ['visa_address'],
+        'classes': ['CovesionOC'],
+        'imports': ['pyvisa'],
+        'visa_info': None,
     }),
     ('tempcontrollers.hcphotonics', {
         'params': [u'visa_address'],
