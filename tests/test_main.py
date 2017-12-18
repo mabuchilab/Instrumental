70817881f4ed0897fc24ec00cb6c182482fdbd2d
Nate Bogdanowicz <natezb@gmail.com>
2017-12-18 15:47:26 -0500

    Add test of instrumental star-import

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,11 @@
 from instrumental import list_instruments
 
+
 def test_list_instruments():
     list_instruments()
+
+
+def test_import_all():
+    import instrumental
+    for attr in dir(instrumental):
+        getattr(instrumental, attr)
