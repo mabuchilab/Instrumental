7f01079fd64e285c149d3a28bab2b3d28a4665eb
Nate Bogdanowicz <natezb@gmail.com>
2017-12-18 16:14:06 -0500

    Ignore failed imports due to missing deps in test
    The lazy import system was put in place to allow a flat import structure without
    requiring e.g. matplotlib to be installed. This fixes `test_import_all` to
    ignore failures due to ImportErrors, but should still catch missing names, which
    was the original intention.

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,4 +8,7 @@ def test_list_instruments():
 def test_import_all():
     import instrumental
     for attr in dir(instrumental):
-        getattr(instrumental, attr)
+        try:
+            getattr(instrumental, attr)
+        except ImportError:
+            pass  # Ignore dependencies on matplotlib, numpy, etc.
