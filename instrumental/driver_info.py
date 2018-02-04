f7db1b08508983a807c573d596e36097102344c3
Nate Bogdanowicz <natezb@gmail.com>
2018-02-04 12:44:58 -0800

    Regenerate driver_info.py

--- instrumental/driver_info.py
+++ instrumental/driver_info.py
@@ -1,4 +1,4 @@
-# Auto-generated 2017-12-14T13:07:32.702000
+# Auto-generated 2018-02-04T11:58:34.235988
 from collections import OrderedDict
 
 driver_info = OrderedDict([
@@ -25,7 +25,7 @@ driver_info = OrderedDict([
     ('cameras.uc480', {
         'params': ['serial', 'id', 'model'],
         'classes': ['UC480_Camera'],
-        'imports': ['win32event', 'nicelib'],
+        'imports': ['pywin32', 'nicelib >= 0.4'],
     }),
     ('daq.ni', {
         'params': ['name', 'serial', 'model'],
@@ -80,7 +80,7 @@ driver_info = OrderedDict([
         'params': ['visa_address'],
         'classes': [],
         'imports': ['visa', 'pyvisa'],
-        'visa_info': {'TDS_2000': ('TEKTRONIX', ['TDS 2002B', 'TDS 2004B', 'TDS 2012B', 'TDS 2014B', 'TDS 2022B', 'TDS 2024B']), 'TDS_1000': ('TEKTRONIX', ['TDS 1001B', 'TDS 1002B', 'TDS 1012B']), 'TDS_3000': ('TEKTRONIX', ['TDS 3012', 'TDS 3012B', 'TDS 3012C', 'TDS 3014', 'TDS 3014B', 'TDS 3014C', 'TDS 3032', 'TDS 3032B', 'TDS 3032C', 'TDS 3034', 'TDS 3034B', 'TDS 3034C', 'TDS 3052', 'TDS 3052B', 'TDS 3052C', 'TDS 3054', 'TDS 3054B', 'TDS 3054C']), 'MSO_DPO_4000': ('TEKTRONIX', ['MSO4032', 'DPO4032', 'MSO4034', 'DPO4034', 'MSO4054', 'DPO4054', 'MSO4104', 'DPO4104']), 'TDS_200': ('TEKTRONIX', ['TDS 210', 'TDS 220', 'TDS 224'])},
+        'visa_info': {'TDS_200': ('TEKTRONIX', ['TDS 210', 'TDS 220', 'TDS 224']), 'TDS_1000': ('TEKTRONIX', ['TDS 1001B', 'TDS 1002B', 'TDS 1012B']), 'TDS_2000': ('TEKTRONIX', ['TDS 2002B', 'TDS 2004B', 'TDS 2012B', 'TDS 2014B', 'TDS 2022B', 'TDS 2024B']), 'TDS_3000': ('TEKTRONIX', ['TDS 3012', 'TDS 3012B', 'TDS 3012C', 'TDS 3014', 'TDS 3014B', 'TDS 3014C', 'TDS 3032', 'TDS 3032B', 'TDS 3032C', 'TDS 3034', 'TDS 3034B', 'TDS 3034C', 'TDS 3052', 'TDS 3052B', 'TDS 3052C', 'TDS 3054', 'TDS 3054B', 'TDS 3054C']), 'MSO_DPO_4000': ('TEKTRONIX', ['MSO4032', 'DPO4032', 'MSO4034', 'DPO4034', 'MSO4054', 'DPO4054', 'MSO4104', 'DPO4104'])},
     }),
     ('spectrometers.bristol', {
         'params': ['port'],
@@ -99,8 +99,8 @@ driver_info = OrderedDict([
         'visa_info': None,
     }),
     ('tempcontrollers.hcphotonics', {
-        'params': [u'visa_address'],
-        'classes': [u'TC038'],
+        'params': ['visa_address'],
+        'classes': ['TC038'],
         'imports': ['pyvisa'],
         'visa_info': None,
     }),
