631e5f18b21734258d28928c4d80ddf13c15d7b7
Nate Bogdanowicz <natezb@gmail.com>
2018-02-20 01:32:28 -0800

    Prepare for release 0.5

--- instrumental/__about__.py
+++ instrumental/__about__.py
@@ -4,7 +4,7 @@
 import datetime
 
 __distname__ = "Instrumental-lib"
-__version__ = "0.5.dev1"
+__version__ = "0.5"
 __author__ = "Nate Bogdanowicz"
 __email__ = "natezb@gmail.com"
 __url__ = 'https://github.com/mabuchilab/Instrumental'
