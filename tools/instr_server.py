294b9d6ab0a6a6c14470944390d5b693504b2d71
Nate Bogdanowicz <natezb@gmail.com>
2018-11-07 22:48:59 -0800

    Update logging in instr_server to use new system

--- tools/instr_server.py
+++ tools/instr_server.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015 Nate Bogdanowicz
+# Copyright 2015-2018 Nate Bogdanowicz
 """
 Instrumental server script. Allows other machines to access and control this machine's instruments.
 """
 import threading
-import logging as log
+import logging
+from instrumental.log import log_to_screen, DEBUG, WARNING
 from instrumental.drivers.remote import ThreadedTCPServer, DEFAULT_PORT
 
 if __name__ == "__main__":
-    log.basicConfig(level=log.DEBUG, format='%(filename)s/%(funcName)s: %(message)s')
+    log_to_screen(level=DEBUG, fmt='[%(levelname)8s]%(filename)s/%(funcName)s: %(message)s')
+    logging.getLogger('nicelib').setLevel(WARNING)
+
     HOST = ''  # Listen on all network interfaces
     server = ThreadedTCPServer((HOST, DEFAULT_PORT))
     ip, port = server.server_address
