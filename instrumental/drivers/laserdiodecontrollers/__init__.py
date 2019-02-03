9bd69b672ed96247bf1475b0a1e18f24df32a454
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2019-02-03 15:05:04 -0800

    Added instruments from Stanford University's Digonnet Lab

---
+++ instrumental/drivers/laserdiodecontrollers/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Package containing a driver module/class for each supported laser diode
+controller.
+"""
+from .. import Instrument
+
+
+class LaserDiodeController(Instrument):
+    pass
