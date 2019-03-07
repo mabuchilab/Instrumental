142fb20c7cca61ad1eecf613c4d05dafd17d7885
Nate Bogdanowicz <natezb@gmail.com>
2019-03-06 18:58:10 -0800

    Clean up formatting in motion.apt

--- instrumental/drivers/motion/apt.py
+++ instrumental/drivers/motion/apt.py
@@ -1,48 +1,49 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Sylvain Pelissier, Nate Bogdanowicz
 import time
+import struct
 
 from serial import Serial
 from serial.tools.list_ports import comports
-from instrumental.drivers.motion import Motion
-from instrumental.drivers import ParamSet
-from ... import Q_, u
 
-import struct
+from . import Motion
+from .. import ParamSet
+from ... import Q_
+
 
 def list_instruments():
     return [ParamSet(TDC001_APT, port=p.device) for p in comports() if (p.vid, p.pid) == (0x0403, 0xfaf0)]
 
+
 class APT(Motion):
-    """
-        Generic class for APT devices.
-    """
+    """Generic Thorlabs device, controlled via APT commands"""
+
     def identify(self):
-        """Identify the device by libkining its LED"""
+        """Identify the device by blinking its LED"""
         self._ser.write(bytes([0x23, 0x02, 0x00, 0x00, self.dst, self.src]))
-    
+
     def get_info(self):
         self._ser.write(bytes([0x05, 0x00, 0x00, 0x00, self.dst, self.src]))
         rsp = self._ser.read(90)
         return rsp
 
-class TDC001_APT(APT):
-    """
-        Controlling Thorlabs TDC001 T-Cube DC Servo Motor Controllers using APT commands
-    """
 
+class TDC001_APT(APT):
+    """Thorlabs TDC001 T-Cube DC Servo Motor Controller, controlled via APT commands"""
     _INST_PARAMS_ = ['serial']
-    
+
     pos_scaling_factor = Q_(1/34304.0, "mm")
     vel_scaling_factor = Q_(1/767367.49, "mm/s")
     acc_scaling_factor = Q_(1/261.93, "mm / s**2")
-    
+
     def _initialize(self):
         self.src = 0x01             # Host controller by default
         self.dst = 0x50             # Generic USB hardware unit
         self._ser = Serial(self._paramset['port'], baudrate=115200, timeout=0.2)
-        
+
     def close(self):
         self._ser.close()
-    
+
     def jog_move(self, direction= 1):
         self._ser.write(bytes([0x6A, 0x04, 0x01, direction, self.dst, self.src]))
         rsp = b""
@@ -50,15 +51,14 @@ class TDC001_APT(APT):
             rsp = self._ser.read(20)
             time.sleep(0.1)
         return rsp
-    
+
     def get_position(self):
-        """ Returns the position of the motor.
-        """
+        """Return the position of the motor"""
         self._ser.write(bytes([0x11, 0x04, 0x01, 0x00, self.dst, self.src]))
         rsp = self._ser.read(12)
         position = struct.unpack('<i', rsp[-4:])[0] * self.pos_scaling_factor
         return position
-    
+
     def get_jog_params(self):
         self._ser.write(bytes([0x17, 0x04, 0x01, 0x00, self.dst, self.src]))
         rsp = self._ser.read(28)
@@ -68,12 +68,12 @@ class TDC001_APT(APT):
         return jog_step_size, jog_accel, jog_max_accel
 
     def get_vel_params(self):
-        """ Gets the velocity parameters for the specified motor
+        """Get the velocity parameters for the specified motor
 
         Returns
         -------
-        (accel: int,
-        vel_max: int)
+        accel: int
+        vel_max: int
         """
         self._ser.write(bytes([0x14, 0x04, 0x01, 0x00, self.dst, self.src]))
         rsp = self._ser.read(20)
@@ -82,17 +82,14 @@ class TDC001_APT(APT):
         return accel, vel_max
 
     def get_enccounter(self):
-        """ Gets the encoder count in the controller.
-        """
+        """Get the encoder count in the controller """
         self._ser.write(bytes([0x0A, 0x04, 0x01, 0x00, self.dst, self.src]))
         rsp = self._ser.read(12)
         enccounter = struct.unpack('<i', rsp[-4:])
         return enccounter
 
     def home(self):
-        """ Homes the device and wait until a move completed
-            message is reveiced.
-        """
+        """Home the device and wait until a move completed message is received"""
         self._ser.write(bytes([0x43, 0x04, 0x01, 0x00, self.dst, self.src]))
         rsp = b""
 
