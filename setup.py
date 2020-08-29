5b29a41cfaf2105ebdd41176f102572915d4de74
Nate Bogdanowicz <natezb@gmail.com>
2020-08-29 16:50:43 -0700

    Add classifiers for Python 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ classifiers = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
 ]
 
 # Load metadata from __about__.py
